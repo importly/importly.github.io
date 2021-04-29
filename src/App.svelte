740f07566354d5568bbf75125a4bc830855a3d89
importly <aryankuthakur@hotmail.com>
2021-04-29 18:57:33 -0400

    animation

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,14 @@
-<script>
-	let name = 'world';
-</script>
 
-<h1>Hello {name}!</h1>
+<div class="navbar">
+	<img src="favicon.png" alt="icon" width ="50">
+	<a href="/about">About</a>
+    <a href="/projects">Projects</a>
+    <a href="/roblox">Roblox</a>
+    <a href="/contact">Contact</a>
+</div> 
+
+<!-- wut -->
+	<!-- 
+        |Icon|  About | Projects | Roblox | Contact
+		that is about it
+    --> 
