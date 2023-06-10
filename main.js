677ad95cccb651824fb60123254bef37ae88e42a
Aryan <52215513+importly@users.noreply.github.com>
2023-06-09 21:03:32 -0400

    added spinning

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from "https://unpkg.com/three@0.153.0/build/three.module.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -16,7 +16,8 @@ camera.position.z = 5;
 
 function animate() {
 	requestAnimationFrame( animate );
-
+	cube.rotation.x += 0.01;
+	cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 
